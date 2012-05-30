f1eea58c3e9d7404074883690b0097a7eab972d5
Kyle Wilcox <kwilcox@asascience.com>
2012-05-30 12:13:58 -0400

    Remove unneeded comment

--- pyoos/cdm/collections/point_collection.py
+++ pyoos/cdm/collections/point_collection.py
@@ -25,12 +25,3 @@ class PointCollection(FeatureCollection):
         self.depth_range = sorted(map(lambda x: x[1].z, stuff))
         self.bbox = MultiPoint(points).envelope
         self.size = len(self._elements)
-
-        """(-74.0, 40.0, -70.0, 44.0)
-            .(-74.0, 40.0, -70.0, 44.0)
-            .(-121.0, 49.0, -120.0, 50.0)
-            .(-120.0, 50.0, -120.0, 50.0)
-            .(-180.0, -90.0, -120.0, 50.0)
-            .(-76.77600421073319, 38.207251289912215, -67.24931948439897, 46.43587593405386)
-            ...(-74.0, 40.0, -70.0, 44.0)
-            ."""
