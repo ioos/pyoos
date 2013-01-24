e8cf736621d5325f51ae293f811ddf8831a00c51
Kyle Wilcox <kwilcox@asascience.com>
2013-01-24 12:02:53 -0500

    Add a time and variable filter on a PointCollection

--- pyoos/cdm/collections/point_collection.py
+++ pyoos/cdm/collections/point_collection.py
@@ -1,6 +1,6 @@
 from pyoos.cdm.collections.feature_collection import FeatureCollection
 from shapely.geometry import MultiPoint
-from shapely.geometry import Point, box
+from datetime import datetime
 
 class PointCollection(FeatureCollection):
     """
@@ -9,6 +9,17 @@ class PointCollection(FeatureCollection):
 
     def __init__(self, **kwargs):
         super(PointCollection,self).__init__(**kwargs)
+
+    def filter_by_time(self, starting, ending):
+        if not isinstance(starting, datetime) or not isinstance(ending, datetime):
+            raise ValueError("Starting and ending must both be datetime objects")
+        return (e for e in self.elements if starting <= e.time <= ending)
+
+    def filter_by_variable(self, variable, elements=None):
+        if elements is None:
+            elements = self.elements
+
+        return (m for m in elements for h in m.members if h.get("name","") == variable or h.get("standard","") == variable)
 
     def calculate_bounds(self):
         """
