3a7169411fe3bb311bc37be5c249c32264cc1445
Alexander Crosby <acrosby@asascience.com>
2013-06-06 15:03:34 -0400

    adding NWA Aviation Weather Center collector, tests, and parser

---
+++ tests/collectors/test_awc_rest.py
@@ -0,0 +1,23 @@
+import unittest
+from pyoos.collectors.awc.awc_rest import AwcRest
+from paegan.cdm.dsg.collections.station_collection import StationCollection
+
+class AwcRestTest(unittest.TestCase):
+
+    def setUp(self):
+        self.c = AwcRest()
+
+    def test_nwc_stations(self):
+        stations = self.c.stations
+        assert stations[0] == 'AGGH'
+        assert stations[-1] == 'ZYTX'
+        
+    def test_bbox_filter_raw(self):
+        self.c.filter(bbox=(-80, 30, -60, 50))
+        response = self.c.raw()
+        assert '<response xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XML-Schema-instance" version="1.2" xsi:noNamespaceSchemaLocation="http://aviationweather.gov/adds/schema/metar1_2.xsd">' in response
+        
+    def test_bbox_filter_paegan(self):
+        self.c.filter(bbox=(-80, 30, -60, 50))
+        response = self.c.collect()
+        assert type(response) == StationCollection
