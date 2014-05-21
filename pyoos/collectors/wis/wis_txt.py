ea2913eead2ea8d03c7902c2899ddad272e4ad75
Andy Bird <abird@asascience.com>
2014-05-21 14:22:58 -0400

    inital commit of wis parser

---
+++ pyoos/collectors/wis/wis_txt.py
@@ -0,0 +1,37 @@
+from pyoos.collectors.collector import Collector
+from datetime import datetime
+import requests
+
+class WisTxt(Collector):
+	def __init__(self, **kwargs):
+		raise StandardError("Not yet implemented....")
+
+	def set_state(self, state):	
+		raise StandardError("Not yet implemented....")
+
+	def get_state(self):
+		raise StandardError("Not yet implemented....")
+
+	def list_variables(self):		
+		raise StandardError("Not yet implemented....")
+
+	def list_features(self):
+		raise StandardError("Not yet implemented....")
+
+	def collect(self):
+		'''
+		Collect
+		'''	
+		raise StandardError("Not yet implemented....")	
+
+	def raw(self, **kwargs):
+		'''
+		Raw
+		'''
+		raise StandardError("Not yet implemented....")
+
+	def metadata(self, **kwargs):
+		'''
+		MetaData
+		'''
+		raise StandardError("Not yet implemented....")		
