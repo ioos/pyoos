91807d32ab2ad019f8339cb4785d1edcfc655d2b
Emilio Mayorga <emiliomayorga@gmail.com>
2014-09-12 20:40:58 -0700

    New utility file and function for generic, useful data reorganization

---
+++ pyoos/utils/dataorg.py
@@ -0,0 +1,14 @@
+
+def flatten_element(p):
+    """
+    Convenience function to return record-style time series representation
+    from elements ('p') members in station element.
+    member['standard'] is a standard_name parameter name, typically CF based.
+    Ideally, member['value'] should already be floating point value,
+    so it's ready to use.
+    Useful with most pyoos collectors.
+    """
+    rd = {'time': p.time}
+    for member in p.members:
+        rd[member['standard']] = member['value']
+    return rd
