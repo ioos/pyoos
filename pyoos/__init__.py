805911470e0ac56888e691cfa02097b1a34efc8e
Kyle Wilcox <kyle@axiomdatascience.com>
2015-01-23 14:47:16 -0500

    Add a named project level logger.  `from pyoos import logger` plz.

--- pyoos/__init__.py
+++ pyoos/__init__.py
@@ -1,1 +1,14 @@
 __version__ = '0.6.2'
+
+# Package level logger
+import logging
+try:
+    # Python >= 2.7
+    from logging import NullHandler
+except ImportError:
+    # Python < 2.7
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+logger = logging.getLogger("pyoos")
+logger.addHandler(logging.NullHandler())
